681ee89816b56ef9021e70d54a734e73a04fe49b
Bharat Singhani <singhani.bharat7@gmail.com>
2025-06-28 02:47:22 +0530

    Implemented get user and feed apis

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(express.json());
 
+// Signup
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
 
@@ -14,6 +15,32 @@ app.post("/signup", async (req, res) => {
     res.send("User added successfully");
   } catch (err) {
     res.status(400).send("Failed to add the user: " + err.message);
+  }
+});
+
+// Get user by email
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.findOne({ emailId: req.body.emailId });
+
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// Get all users form the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Something went wrong");
   }
 });
 
