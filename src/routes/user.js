37b7a5cd791739bdc317890b58e79e11559da928
Bharat Singhani <singhani.bharat7@gmail.com>
2025-06-29 23:20:51 +0530

    Implemented review requests and get all connections apis using ref and populate

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,42 @@
 const express = require("express");
 
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
 
 const userRouter = express.Router();
+const USER_SAFE_DATA = "firstName lastName photoURL skills age about";
 
 // Get connections
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connections = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    }).populate("fromUserId", USER_SAFE_DATA);
 
-// Get recieved requests
+    res.send(connections.map((field) => field.fromUserId));
+  } catch (error) {
+    res.status(400).send("Error: " + error.message);
+  }
+});
+
+// Get al the pending connection requests for the looged in user
+userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const pendingRequests = await ConnectionRequest.find({
+      toUserId: loggedInUser._id,
+      status: "interested",
+    }).populate("fromUserId", USER_SAFE_DATA);
+
+    res.send(pendingRequests);
+  } catch (error) {
+    res.status(400).send("Error: " + error.message);
+  }
+});
 
 // Get the feed(recommendations)
 
