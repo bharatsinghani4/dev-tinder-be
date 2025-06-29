9611f579c913d9ea8dad3cb9e800e0ef453a67e5
Bharat Singhani <singhani.bharat7@gmail.com>
2025-06-30 03:25:08 +0530

    Implemented feed api using Sets and select schema method

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const { userAuth } = require("../middlewares/auth");
+const User = require("../models/user");
 const ConnectionRequest = require("../models/connectionRequest");
 
 const userRouter = express.Router();
@@ -15,9 +16,19 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
         { toUserId: loggedInUser._id, status: "accepted" },
         { fromUserId: loggedInUser._id, status: "accepted" },
       ],
-    }).populate("fromUserId", USER_SAFE_DATA);
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
 
-    res.send(connections.map((field) => field.fromUserId));
+    res.send(
+      connections.map((field) => {
+        if (field.fromUserId._id.toString() === loggedInUser._id.toString()) {
+          return field.toUserId;
+        }
+
+        return field.fromUserId;
+      })
+    );
   } catch (error) {
     res.status(400).send("Error: " + error.message);
   }
@@ -39,5 +50,26 @@ userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
 });
 
 // Get the feed(recommendations)
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const existingConnectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    });
+    // These will contain ids of the logged in user as well as of those with whom it has a connection request associated
+    const hideUsersFromFeed = new Set();
+    existingConnectionRequests.forEach((request) => {
+      hideUsersFromFeed.add(request.fromUserId.toString());
+      hideUsersFromFeed.add(request.toUserId.toString());
+    });
+    const feedUsers = await User.find({
+      _id: { $nin: Array.from(hideUsersFromFeed) }, // converting set into Array
+    }).select(USER_SAFE_DATA);
+
+    res.send(feedUsers);
+  } catch (error) {
+    res.status(400).send("Error: " + error.message);
+  }
+});
 
 module.exports = userRouter;
