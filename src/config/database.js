90b4ed553ed087038b3d596c8c305548e408222a
Bharat Singhani <singhani.bharat7@gmail.com>
2026-03-07 19:41:23 +0530

    Removed all files for revision

--- src/config/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  await mongoose.connect(process.env.DB_CONNECTION_SECRET);
-};
-
-module.exports = connectDB;
--- src/utils/constants.js
+++
@@ -1,6 +0,0 @@
-const membershipAmount = {
-  gold: 800,
-  silver: 400,
-};
-
-module.exports = { membershipAmount };
