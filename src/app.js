b6827cbc206fcded02d1d65d09ee6f6536a7ef2a
Bharat Singhani <singhani.bharat7@gmail.com>
2026-03-10 17:52:59 +0530

    S02E05 - Middlewares and Error Handlers

--- src/app.js
+++ src/app.js
@@ -2,22 +2,31 @@ const express = require("express");
 
 const app = express();
 
-app.get(/.*fly$/, (req, res) => {
-  res.send({
-    firstName: "Bharat",
-    lastName: "Singhani",
-  });
-});
-
-app.use("/user?userId=xyz", (req, res) => {
-  console.log(req.query);
-  res.send("Hello from the server");
-});
-
-app.use("/user/:userId/:name", (req, res) => {
-  console.log(req.params);
-  res.send("Hello from the server");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Route handler 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Route handler 2");
+    next();
+  },
+  [
+    (req, res, next) => {
+      console.log("Route handler 3");
+      next();
+    },
+    (req, res, next) => {
+      console.log("Route handler 4");
+      next();
+    },
+  ],
+  (req, res, next) => {
+    console.log("Route handler 5");
+    res.send("Response 5");
+  },
+);
 
 app.listen(1304, () => {
   console.log("Server is listening on port 1304...");
